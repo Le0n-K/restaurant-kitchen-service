cc1e3afb9c2fe6d6752004eb5d692db4ccc65eea
Leon <leon.kushnir15@gmail.com>
2024-12-02 12:15:48 +0200

    Migrate AbstractUser

--- kitchen/models.py
+++ kitchen/models.py
@@ -6,6 +6,12 @@ from django.db import models
 class DishType(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self) -> str:
+        return str(self.name)
+
 
 class Dish(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -13,3 +19,9 @@ class Dish(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
     chefs = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="chefs")
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self) -> str:
+        return f"{self.name}: {self.price}"
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "restaurant",
     "kitchen",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -124,4 +125,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "kitchen.Chef"
+AUTH_USER_MODEL = "accounts.Chef"
