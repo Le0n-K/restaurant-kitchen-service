d5da85437dc97f1a4d9ba863f2942f758a56bd22
Leon <leon.kushnir15@gmail.com>
2024-12-06 16:23:37 +0200

    add build.sh and separated settings

--- restaurant/settings/base.py
+++ restaurant/settings/base.py
@@ -48,17 +48,6 @@ INSTALLED_APPS = [
     # "nested_forms",
 ]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
 ROOT_URLCONF = "restaurant.urls"
 
 TEMPLATES = [
